0e9433d087b4d1d571336637031fdf77a7dcad82
Freddy <fredericdouamba2014@gmail.com>
2023-10-20 11:19:31 -0700

    Chapter20: GeometryReader

--- GeometryReader/ContentView.swift
+++ GeometryReader/ContentView.swift
@@ -9,16 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GeometryReader { geometry in
+            ZStack {
+               
+                Text("Background: paper")
+                    .font(.largeTitle)
+                    .frame(width: geometry.size.width * 0.9, alignment: .leading)
+                    .position(x: geometry.size.width * 0.05, y: geometry.size.height / 2)
+
+               
+                Image(systemName: "paperplane")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: geometry.size.height * 0.5)
+                    .opacity(0.35) // Setting opacity to 35% as an example within 20-50% range
+                    .position(x: geometry.size.width * 0.5, y: geometry.size.height * 0.5)
+            }
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
+
+

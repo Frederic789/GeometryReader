a835b320ee98e8023fae29124e5b6dfea80863d6
Freddy <fredericdouamba2014@gmail.com>
2023-10-20 10:49:46 -0700

    Initial Commit

---
+++ GeometryReader/GeometryReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryReaderApp.swift
+//  GeometryReader
+//
+//  Created by Student Account on 10/20/23.
+//
+
+import SwiftUI
+
+@main
+struct GeometryReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
